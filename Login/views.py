a49de0ccafaceacc5ba26843c239c6a3889988bd
erikaher <183420@ids.upchiapas.edu.mx>
2020-02-10 10:22:34 -0600

    add-serializers

--- Login/views.py
+++ Login/views.py
@@ -1,6 +1,28 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+
+#Siempre que se crea una vista (5-11)
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
+
+# Importar modelo
+from Login.models import Example2
+# Importar Serializer
+from Login.serializer import Example2Serializers
+
+class ExampleList2(APIView):
+    # Metodo get para solicitar info
+    def get(self, request, format=None):
+        print("Metodo get filter")
+        queryset = Example2.objects.filter(delete = False)
+        # many = True Si aplica si retorno multiples objetos
+        serializer = Example2Serializers(queryset, many=True)
+        return Response(serializer.data)
 
 class CustonAuthToken(ObtainAuthToken):
     
