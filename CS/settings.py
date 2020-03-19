3908f2efc867f16af130f793feb276748c3101af
erikaher <183420@ids.upchiapas.edu.mx>
2020-03-18 21:42:39 -0600

    collectstatic

--- CS/settings.py
+++ CS/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
+'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.AllowAny',),
 'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication',),
 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 'PAGE_SIZE': 100,
@@ -144,3 +144,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'assets'
